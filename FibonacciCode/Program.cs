bec1cf128dca83fc9db5607a781a092241cf521c
lonys <lonys@LAPTOP-8GOJV76L>
2023-12-04 09:17:42 +0100

    Add project files.

---
+++ FibonacciCode/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("***");
+Console.WriteLine("The Fibonacci's Sequence");
+Console.WriteLine("***");
+
+int a = 0;
+int b = 1;
+int c;
+
+for (int i = 0; i < 10; i++)
+{
+    c = a + b;
+    a = b;
+    b = c;
+    Console.WriteLine(c);
+
+}
+
